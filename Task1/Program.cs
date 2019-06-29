2ab15774012a3f12ef633633bbf824bbad15f9a6
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-06-29 14:05:39 +0300

    add implicit conversions

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Explicit conversions
+
+      // Implicit conversions
+      byte numb1 = 255;
+      short numb2 = numb1;
+      int numb3 = numb2;
+      long numb4 = numb3;
+      float numb5 = numb4;
+      Console.WriteLine(numb5);
+
+      // Boxing
+
+      // Unboxing
+    }
+  }
+}
