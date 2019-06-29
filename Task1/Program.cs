43140810c00d4edaa437c72ca705a397d9186edf
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-06-29 20:43:37 +0300

    explicit and implicit conversions were added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,14 +8,24 @@ namespace Task1
     {
       // Explicit conversions
 
+      double numb6 = 8195.5;
+      long a;
+      int b;
+      short c;
+      byte d;
+
+      a = (long)numb6;
+      b = (int)numb6;
+      c = (short)numb6;
+      d = (byte)numb6;
 
       // Implicit conversions
+
       byte numb1 = 255;
       short numb2 = numb1;
       int numb3 = numb2;
       long numb4 = numb3;
       float numb5 = numb4;
-      Console.WriteLine(numb5);
 
       // Boxing
 
