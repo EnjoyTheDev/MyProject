0b542c044bf9cb4eb5cc59e3fe0bd8be6306ba03
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-06-29 22:54:33 +0300

    task2 was added

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      object something = "some string";
+      String s = "hello";
+      Double dNumb = 23.15;
+      byte bNumb = 255;
+      Int32 iNumb32 = 58;
+      int iNumb = 670;
+
+      Console.WriteLine($"something -> {something.GetType()}, s -> {s.GetType()}, dNumb -> {dNumb.GetType()}");
+      Console.WriteLine($"bNumb -> {bNumb.GetType()}, iNumb32 -> {iNumb32.GetType()}, iNumb -> {iNumb.GetType()}");
+
+      Console.ReadLine();
+    }
+  }
+}
