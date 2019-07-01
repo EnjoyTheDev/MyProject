d60e1241ab6d2637af4c56c687226e839303e830
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-07-01 08:54:13 +0300

    the sum of two numbers

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task3
+{
+  class Program
+  {
+    static void Sum()
+    {
+
+      Console.WriteLine($"enter first number:");
+      int a = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine($"enter second number:");
+      int b = Convert.ToInt32(Console.ReadLine());
+
+      var result = a + b; 
+      Console.WriteLine($"result = {result}");
+    }
+    static void Main(string[] args)
+    {
+      Sum();
+    }
+  }
+}
