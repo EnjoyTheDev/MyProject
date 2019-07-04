0b756c474874afa2b3ab97b580065408e22258da
Vladislav Seredinsky <44668208+EnjoyTheDev@users.noreply.github.com>
2019-07-04 15:22:17 +0300

    Update Program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,19 +4,86 @@ namespace Task3
 {
   class Program
   {
+using System;
+
+namespace Task3
+{
+  class Program
+  {
+    // Task 1
     static void Sum()
     {
-
+      Console.WriteLine("Task 1");
       Console.WriteLine("enter first number:");
       int a = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("enter second number:");
+      int b = Convert.ToInt32(Console.ReadLine());
+      var result = a + b;
+      Console.WriteLine(result);
+      Console.WriteLine();
+    }
+    //Task 2
+    static void Check()
+    {
+      Console.WriteLine("Task 2");
+      Console.WriteLine("enter first number:");
+      int a = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("enter second number:");
+      int b = Convert.ToInt32(Console.ReadLine());
 
+      Console.WriteLine("enter your result:");
+      var yourResult = Convert.ToInt32(Console.ReadLine());
+      var myResult = a + b;
+
+      if (yourResult == myResult)
+      {
+        Console.WriteLine("Correct");
+      }
+      else
+      {
+        Console.WriteLine("Incorrect ");
+      }
+      Console.WriteLine();
+    }
+    // Task 3
+    static void Comparison()
+    {
+      Console.WriteLine("Task 3");
+      Console.WriteLine("enter first number:");
+      int a = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("enter second number:");
+      int b = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("enter your result:");
+      var yourResult = Convert.ToInt32(Console.ReadLine());
+      var myResult = a + b;
+
+      if (yourResult == myResult)
+      {
+        Console.WriteLine("Correct");
+      }
+      else if (yourResult > myResult)
+      {
+        Console.WriteLine("Incorrect - should be less");
+      }
+      else if (yourResult < myResult)
+      {
+        Console.WriteLine("Incorrect - should be more");
+      }
+      Console.WriteLine();
+    }
+    // Task 4
+    static void Choice()
+    {
+      Console.WriteLine("Task 4");
+      Console.WriteLine("enter first number:");
+      int a = Convert.ToInt32(Console.ReadLine());
       Console.WriteLine("enter second number:");
       int b = Convert.ToInt32(Console.ReadLine());
 
       Console.WriteLine("enter + or -");
       var select = Console.ReadLine();
       var myResult = select == "+" ? (a + b) : (a - b);
-
 
       Console.WriteLine("enter your result:");
       var yourResult = Convert.ToInt32(Console.ReadLine());
@@ -28,7 +95,6 @@ namespace Task3
       else if (yourResult > myResult)
       {
         Console.WriteLine("Incorrect - should be less");
-
       }
       else if (yourResult < myResult)
       {
@@ -38,6 +104,12 @@ namespace Task3
     static void Main(string[] args)
     {
       Sum();
+      Check();
+      Comparison();
+      Choice();
     }
+  }
+}
+
   }
 }
