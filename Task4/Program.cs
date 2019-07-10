2cfa8a0ceb1c3137242b5093e76562960225e3e2
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-07-10 22:31:02 +0300

    done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,16 +4,61 @@ namespace Task4
 {
   class Program
   {
+    // Task 1
+    static void GetReverseLetter()
+    {
+      var firstLetter = 'A';
+      for (var lastLetter = 'Z'; firstLetter <= lastLetter; lastLetter--)
+      {
+        Console.WriteLine(lastLetter);
+      }
+    }
+    // Task 2
+    static void LetsPlay()
+    {
+      ConsoleKeyInfo keypress;
+      Console.WriteLine("Moving the character. W - forward, S - backward, A - left, D - right");
+      do
+      {
+        keypress = Console.ReadKey(true);
+        if (keypress.KeyChar == 'W' || keypress.KeyChar == 'w')
+        {
+          Console.WriteLine("Forward");
+        }
+        if (keypress.KeyChar == 'S' || keypress.KeyChar == 's')
+        {
+          Console.WriteLine("Back");
+        }
+        if (keypress.KeyChar == 'A' || keypress.KeyChar == 'a')
+        {
+          Console.WriteLine("Left");
+        }
+        if (keypress.KeyChar == 'D' || keypress.KeyChar == 'd')
+        {
+          Console.WriteLine("Rigth");
+        }
+      }
+      while (keypress.KeyChar == 'A' || keypress.KeyChar == 'W' || keypress.KeyChar == 'S' || keypress.KeyChar == 'D' || keypress.KeyChar == 'a' || keypress.KeyChar == 'w' || keypress.KeyChar == 's' || keypress.KeyChar == 'd');
+
+      Console.WriteLine("You were lost, you can only move left, right, back, forward.");
+      Console.WriteLine("Play again? Y/N");
+      var confirm = Console.ReadKey(true);
+
+      if (confirm.KeyChar == 'Y' || confirm.KeyChar == 'y')
+      {
+        LetsPlay();
+      }
+      else if (confirm.KeyChar == 'N' || confirm.KeyChar == 'n')
+      {
+        return;
+      }
+      Console.Read();
+    }
+
     static void Main(string[] args)
     {
       GetReverseLetter();
-    }
-    static void GetReverseLetter()
-    {
-      for (int item = 90; item >= 65; item--)
-      {
-        Console.WriteLine(Convert.ToChar(item));
-      }
+      LetsPlay();
     }
   }
 }
